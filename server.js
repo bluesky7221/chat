1f44a2e5a5e79ff1b2c4b7d553a6413e24b42289
EWC <bluesky7221@naver.com>
2016-07-10 09:56:21 +0900

    amqp save

--- server.js
+++ server.js
@@ -1,29 +1,40 @@
 var express = require('express');
 var app = express();
-var http = require('http').Sever(app);
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var amqp = require('amqp');
 
 app.get("/", function(req,res){
   res.sendfile("client.html");
 });
 
-var count-1;
-io.on('connection', function(socket){
-  console.log('user connected: ', socket.id);
-  var name = "user" + count++;
-  io.to(socket.id).emit('change name', name);
+var count=1;
+var connection = amqp.createConnection({host: 'localhost'});
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected: ', socket.id);
-  });
+connection.on('error', function(e)
+{
+  console.log("Error from amqp: ", e);
+});
 
-  socket.on('send message', function(name,text){
-    var msg = name + ' : ' + text;
-    console.log(msg);
-    io.emit('receive message', msg);
+connection.on('ready', function(){
+  io.on('connection', function(socket){
+    console.log('user connected: ', socket.id);
+    var name = "user" + count++;
+    io.to(socket.id).emit('change name', name);
+
+    socket.on('disconnect', function(){
+      console.log('user disconnected: ', socket.id);
+    });
+
+    socket.on('send message', function(name,text){
+      var msg = name + ' : ' + text;
+      console.log(msg);
+      io.emit('receive message', msg);
+    });
   });
 });
 
-http.listen('3003', function(){
+
+http.listen('3001', function(){
   console.log("server on!");
 });
